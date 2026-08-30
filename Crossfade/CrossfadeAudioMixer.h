b536a064d10096806dc7ff97baecf0d5fa02f4fa
simongellesch4726-hash <simongellesch4726@gmail.com>
2026-08-30 15:12:26 +0200

    Expose mixer playback state controls

--- Crossfade/CrossfadeAudioMixer.h
+++ Crossfade/CrossfadeAudioMixer.h
@@ -8,6 +8,8 @@
 - (BOOL)prepareIncomingPlayer:(id)incoming;
 - (void)setPrimaryGain:(float)gain;
 - (void)setIncomingGain:(float)gain;
+- (void)setPrimaryActive:(BOOL)active;
+- (void)setIncomingActive:(BOOL)active;
 - (void)stopIncomingPlayer:(id)incoming;
 - (void)detach;
 
